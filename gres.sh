3048805a8ee214be32d5ef0d93214827e73e19f5
davidell <david_ell@ml.com>
2017-04-22 12:53:11 -0400

    Update gres.sh

--- gres.sh
+++ gres.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in $(find / -type f -name "*.dockerreplace");
 do
-envsubst < $i > $(echo $i | sed 's/.dockerreplace//')
+  envsubst < $i > $(echo $i | sed 's/.dockerreplace//')
 done
